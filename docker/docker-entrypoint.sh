b7a858e58ba6099ba03621f97d2ee3a688950a0b
Alexandre Fenyo <alexandre.fenyo@gmail.com>
2025-01-12 19:26:15 +0100

    new

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,3 +1,23 @@
+#!/bin/sh
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- haproxy "$@"
+fi
+
+if [ "$1" = 'haproxy' ]; then
+	shift # "haproxy"
+	# if the user wants "haproxy", let's add a couple useful flags
+	#   -W  -- "master-worker mode" (similar to the old "haproxy-systemd-wrapper"; allows for reload via "SIGUSR2")
+	#   -db -- disables background mode
+	set -- haproxy -W -db "$@"
+fi
+
+exec "$@"
+
+exit 0
+
 #!/bin/bash
 set -e
 
@@ -11,5 +31,13 @@ cat /tmp/haproxy.cfg
 echo -- ------------------------------------------------------------
 
 # Exécuter l'exécutable HAProxy
+
+#haproxy -f /tmp/haproxy.cfg
+
+#echo FIN
+#exit 0
+
 echo exec "$@"
-exec "$@" -d
+exec "$@"
+
+############################################################
